1809738387efce2764b55b8a2bf87448762ea3ac
RunningWolfling <138510739+RunningWolfling@users.noreply.github.com>
2023-11-22 23:41:49 +0300

    Update filters.py

--- NewsPaper/news/filters.py
+++ NewsPaper/news/filters.py
@@ -5,7 +5,7 @@ from django import forms
 
 class PostFilter(FilterSet):
     head = django_filters.CharFilter(field_name='head', label='Поиск', lookup_expr='icontains', )
-    author = django_filters.ModelChoiceFilter(field_name='author', label='Выбрать автора', lookup_exact='exact', queryset=Author.objects.all())
+    author = django_filters.ModelChoiceFilter(field_name='author', label='Выбрать автора', lookup_expr='exact', queryset=Author.objects.all())
     time_in = django_filters.DateFilter(field_name='time_create', widget=forms.DateInput(attrs={'type': 'date'}), label='Дата', lookup_expr='date__gte')
     class Meta:
         model = Post
